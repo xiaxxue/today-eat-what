8c33ca5f638617d160d56a4d371a22e166fabc06
xiax <snowy200103@163.com>
2026-08-23 00:57:46 +0800

    Deploy WeChat verification file

--- build-static.js
+++ build-static.js
@@ -5,5 +5,9 @@ const outputDir = join(__dirname, 'dist');
 mkdirSync(outputDir, { recursive: true });
 copyFileSync(join(__dirname, 'index.html'), join(outputDir, 'index.html'));
 copyFileSync(join(__dirname, 'app.js'), join(outputDir, 'app.js'));
+copyFileSync(
+  join(__dirname, '10b7c4affc7aba2d71789e7834748003.txt'),
+  join(outputDir, '10b7c4affc7aba2d71789e7834748003.txt'),
+);
 
 console.log('static site built in dist/');
